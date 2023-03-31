ff951d8a1b901825314639455bb6ec6c3b41eb9b
Laura <laura.tracierra@gmail.com>
2023-03-31 18:22:11 -0300

    feat: add tweet validations

--- src/main/java/com/tweteroo/api/dto/TweetDTO.java
+++ src/main/java/com/tweteroo/api/dto/TweetDTO.java
@@ -1,8 +1,18 @@
 package com.tweteroo.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record TweetDTO(
+    @NotBlank
     String username,
+
+    @Pattern(regexp = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
+    @NotNull
     String avatar,
+
+    @NotBlank
     String text
 ) {
     
