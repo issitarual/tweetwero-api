7c04e0ae9f1bfd6ca77880c6da6a5262e0c7aa6e
Laura <laura.tracierra@gmail.com>
2023-03-31 18:25:23 -0300

    feat: add user validations

--- src/main/java/com/tweteroo/api/dto/UserDTO.java
+++ src/main/java/com/tweteroo/api/dto/UserDTO.java
@@ -1,7 +1,15 @@
 package com.tweteroo.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record UserDTO(
+    @NotBlank
     String username,
+
+    @Pattern(regexp = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
+    @NotNull
     String avatar
 ) {
     
