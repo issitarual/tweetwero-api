d0730c43acfbfda270d50a2ee2c69384d6b9892f
Laura <laura.tracierra@gmail.com>
2023-03-31 18:24:31 -0300

    feat: add user model

---
+++ src/main/java/com/tweteroo/api/model/User.java
@@ -0,0 +1,35 @@
+package com.tweteroo.api.model;
+
+import org.hibernate.validator.constraints.URL;
+
+import com.tweteroo.api.dto.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User {
+    
+    public User(UserDTO data) {
+        this.username = data.username();
+        this.avatar = data.avatar();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 15, nullable = false)
+    private String username;
+
+    @URL
+    @Column (nullable = false)
+    private String avatar;
+}
